01aed9fafa871f07edefe7864f1a5d8192da7beb
Bob Feuerstein <thebobbyllama@yahoo.com>
2020-10-17 19:05:48 -0500

    Removed unused code.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-//const routes = require('./routes');
 
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -30,8 +29,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-//app.use(routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
